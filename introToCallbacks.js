071c46dd5a7aa1f2b3348fa2c94214099b9bc31e
Will Weihnacht <wwhynot@terpmail.umd.edu>
2022-11-23 14:45:37 -0800

    finished addNumbers

--- introToCallbacks.js
+++ introToCallbacks.js
@@ -24,4 +24,25 @@ class Clock {
     }
 }
 
-const clock = new Clock();
+//const clock = new Clock();
+
+const readline = require("readline");
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function addNumbers(sum,numsLeft,completionCallback) {
+    if (numsLeft > 0) {
+        reader.question('Enter a number: ',(input)=>{
+            sum += parseInt(input);
+            console.log(sum);
+            addNumbers(sum,--numsLeft,completionCallback);
+        });
+    } else {
+        completionCallback(sum);
+        reader.close();
+    }
+}
+
+addNumbers(0,3,sum => console.log(`Total Sum: ${sum}`));
